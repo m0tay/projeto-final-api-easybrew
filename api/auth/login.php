a1580bb1a6b6675ccdcc5f435962ec032e5de6d2
Douglas Lobo <douglascnlobo@icloud.com>
2025-11-23 16:27:25 +0000

    Added minor improvements to login.php
    Fixed typos and logic

--- api/auth/login.php
+++ api/auth/login.php
@@ -1,21 +1,23 @@
 <?php
 require_once '../../config.php';
 require_once '../../core.php';
-$pdo = connectDB($db);
-// Carregar classe
-require_once '../../objects/BREAD.php';
 require_once '../../objects/User.php';
-$user = new User($pdo);
-// Carregar JWT
 require '../../vendor/autoload.php';
 
+$pdo = connectDB($db);
+$user = new User($pdo);
+
 use \Firebase\JWT\JWT;
-// Obter dados do POST
-$data = json_decode(file_get_contents("php://input"));
+
+$data = json_decode(file_get_contents('php://input'));
+
+$code = 400;
+$response = ['message' => 'Dados não fornecidos'];
+
 if (!empty($data)) {
-  // Validar dados
-  $user->email = filter_var($data->email, FILTER_SANITIZE_EMAIL);
-  $plain_password = $data->password;
+  $user->email = isset($data->email) ? filter_var($data->email, FILTER_SANITIZE_EMAIL) : '';
+  $plain_password = isset($data->password) ? $data->password : '';
+
   $error = '';
   if ($plain_password == '') {
     $error .= 'Password não definida. ';
@@ -23,46 +25,44 @@ if (!empty($data)) {
   if ($user->email == '') {
     $error .= 'Email não definido. ';
   }
-  if (!$user->emailExists()) {
-    $error .= 'Email não existe. ';
-  }
   if ($error == '') {
-    if (password_verify($plain_password, $user->password_hash)) {
-      // Criar token
-      $token = array(
-        "iss" => $jwt_conf['iss'],
-        "jti" => $jwt_conf['jti'],
-        "iat" => $jwt_conf['iat'],
-        "nbf" => $jwt_conf['nbf'],
-        "exp" => $jwt_conf['exp'],
-        "data" => array(
-          "id" => $user->id,
-          "first_name" => $user->first_name,
-          "last_name" => $user->last_name,
-          "email" => $user->email
-        )
-      );
-      // Criar JWT
-      $jwt = JWT::encode($token, $jwt_conf['key'], 'HS256');
-      // Sucesso na autenticação - 200 OK
-      $code = 200;
-      $response = ['message' => 'Autenticado com sucesso', 'jwt' => $jwt];
-    } else {
-      // Erros no pedido - 401 
+    if (!$user->emailExists()) {
       $code = 401;
-      $response = ['message' => 'Erro de autenticação'];
+      $response = ['message' => 'Credenciais inválidas'];
+    } else {
+      if (!$user->is_active) {
+        $code = 403; // Forbidden BE GONE, PEASANT
+        $response = ['message' => 'Conta desativada'];
+      } else if (password_verify($plain_password, $user->password_hash)) {
+        // Criar token
+        $token = array(
+          'iss' => $jwt_conf['iss'],
+          'jti' => $jwt_conf['jti'],
+          'iat' => $jwt_conf['iat'],
+          'nbf' => $jwt_conf['nbf'],
+          'exp' => $jwt_conf['exp'],
+          'data' => array(
+            'id' => $user->id,
+            'first_name' => $user->first_name,
+            'last_name' => $user->last_name,
+            'email' => $user->email
+          )
+        );
+        // Criar JWT
+        $jwt = JWT::encode($token, $jwt_conf['key'], 'HS256');
+        $code = 200;
+        $response = ['message' => 'Autenticado com sucesso', 'jwt' => $jwt];
+      } else {
+        $code = 401;
+        $response = ['message' => 'Credenciais inválidas'];
+      }
     }
   } else {
-    // Erros no pedido - 400 bad request
     $code = 400;
-    $response = ["message" => $error];
+    $response = ['message' => $error];
   }
-} else {
-  // Erros no pedido - 400 bad request
-  $code = 400;
-  $response = ["message" => "Dados não fornecidos"];
-}
+} 
 
-header("Content-Type: application/json; charset=UTF-8");
+header('Content-Type: application/json; charset=UTF-8');
 http_response_code($code);
 echo json_encode($response);
