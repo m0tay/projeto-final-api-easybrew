05dbf1c4d3c6ad0aa6d899b531bc8cdd0e08d6ff
Douglas Lobo <douglascnlobo@icloud.com>
2025-11-28 00:56:03 +0000

    Included die() as of suggestion

--- api/up.php
+++ api/up.php
@@ -8,4 +8,4 @@ $response = ['message' => 'Up and alive!'];
 header('Content-Type: application/json; charset=UTF-8');
 http_response_code($code);
 echo json_encode($response);
-
+die(); // https://stackoverflow.com/questions/4064444/returning-json-from-a-php-script#comment88774427_4064468
